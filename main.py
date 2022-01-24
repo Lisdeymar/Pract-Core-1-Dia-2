31d03309cfd90f2c9ea25b65a56add868cb641e0
Lisdey Echevarria <lisdeymar07@gmail.com>
2022-01-24 13:09:02 -0500

    2commit

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ alex = CuentaBancaria( 0.01, 100 )
 ana = CuentaBancaria( 0.01, 200 )
 
 alex.depósito(5).depósito(10).depósito(20).retiro(30).generar_interés().mostrar_info_cuenta()
-ana.depósito(40).depósito(50).depósito(60).retiro(70).generar_interés().mostrar_info_cuenta()
+ana.depósito(200).depósito(350).retiro(10).retiro(15).retiro(20).retiro(25).generar_interés().mostrar_info_cuenta()
 
 CuentaBancaria.imprimir_cuentas()
 
